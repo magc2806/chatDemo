54ec4624c4a52fabd4da8861d5e30c41abfbbe77
magc2806 <gonzalezcabellomanuel@gmail.com>
2024-03-26 22:43:35 -0300

    feat: add room scaffold and first turbo_frames

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_18_210256) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_27_000554) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
