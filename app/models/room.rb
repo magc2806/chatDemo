b20654de096bcf66cb922d98eca504f6a50f4254
magc2806 <gonzalezcabellomanuel@gmail.com>
2024-04-16 09:44:34 -0300

    feat: add broadcast on room update

--- app/models/room.rb
+++ app/models/room.rb
@@ -12,4 +12,10 @@ class Room < ApplicationRecord
   has_many :users, through: :user_rooms
 
   validates :name, presence: :true
+
+  after_update_commit :update_room_details
+
+  def update_room_details
+    broadcast_replace_to('room_details_channel', partial: 'shared/room', locals: { room: self }, target: "room_#{id}" )
+  end
 end
